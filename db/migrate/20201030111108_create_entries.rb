d8031cc10fe7c3f7ba584071215abb63e9215e4c
frvalenzuela <frvalenzuela@uc.cl>
2020-10-30 08:11:52 -0300

    add:first-model

---
+++ db/migrate/20201030111108_create_entries.rb
@@ -0,0 +1,13 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.string :meal_type
+      t.integer :calores
+      t.integer :protiens
+      t.interger :carbohydrates
+      t.integer :fats
+
+      t.timestamps
+    end
+  end
+end
